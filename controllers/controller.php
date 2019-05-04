a3c95e7fa76900908f5c2f772b49e528b6ab801a
gibloor <48044665+gibloor@users.noreply.github.com>
2019-05-04 16:50:18 +0300

    Add files via upload

--- controllers/controller.php
+++ controllers/controller.php
@@ -8,7 +8,7 @@ class Controller
 
     public function __construct()
     {
-		    $this->view = new View();
-		    $this->model = new Model();
+$this->view = new View();
+$this->model = new Model();
     }
 }
