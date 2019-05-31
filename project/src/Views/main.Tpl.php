ab39cc4d2157691200bff868c3fd4a961a9fbcd7
gibloor <48044665+gibloor@users.noreply.github.com>
2019-05-31 23:47:03 +0200

    Add files via upload

---
+++ project/src/Views/main.Tpl.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $pageData['title']; ?></title>
+</head>
+<body>
+    <a href="catalog">catalog</a> <br>
+    <a href="adminMenu">adminMenu</a> <br>
+</body>
+</html>
